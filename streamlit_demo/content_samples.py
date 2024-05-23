6b52f45afa738667258e4d226aae4087af1d99b5
juliennekateponce <89396806+juliennekateponce@users.noreply.github.com>
2024-05-24 05:53:33 +0800

    Update content_samples.py

--- streamlit_demo/content_samples.py
+++ streamlit_demo/content_samples.py
@@ -32,6 +32,7 @@ import numpy as np
 
 rand=np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
-ax.hist(rand, bins=15)st.pyplot(fig)
+ax.hist(rand, bins=15)
+st.pyplot(fig)
 
  # See https://www.datacamp.com/tutorial/streamlit to learn more about how to implement more streamlit features in-depth
